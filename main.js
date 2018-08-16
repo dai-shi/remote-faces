ab632dfe752ac720a6aae35aae8df060f82eccff
daishi <daishi@axlight.com>
2018-08-16 23:43:11 +0900

    new prompt feature

--- main.js
+++ main.js
@@ -1,19 +1,58 @@
-const params = Qs.parse(location.hash.slice(1));
+// utils ---------------------
 
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+const hash = x => x && CryptoJS.MD5(x).toString().slice(0, 16);
+
+// params --------------------
+
+const params = Qs.parse(window.location.hash.slice(1));
+const updateParams = () => {
+  window.location.hash = '#' + Qs.stringify(params);
+};
+
+const getRoomid = () => {
+  while (!params.roomid) {
+    params.roomid = prompt('Enter Room ID');
+    if (params.roomid) updateParams();
+  }
+  return hash(params.roomid);
+};
+
+const getMyself = () => {
+  while (!params.myself) {
+    params.myself = prompt('Enter your name');
+    if (params.myself) updateParams();
+  }
+  return hash(params.myself);
+};
+
+const getMembers = () => {
+  while (!Array.isArray(params.members)) {
+    const result = prompt('Enter friend names (comma separated)');
+    if (result) {
+      params.members = result.split(',');
+      updateParams();
+    }
+  }
+  return params.members.map(hash);
+};
+
+// let myid = params.myid;
+
+// ---------------------------
 
 const takePhoto = async () => {
   const stream = await navigator.mediaDevices.getUserMedia({ video: true });
   const canvas = document.querySelector('canvas');
   const ctx = canvas.getContext('2d');
   const track = stream.getVideoTracks()[0];
-  imageCapture = new ImageCapture(track);
+  const imageCapture = new ImageCapture(track);
   await sleep(2000);
-  const bitmap = await imageCapture.grabFrame()
+  const bitmap = await imageCapture.grabFrame();
   track.stop();
   canvas.width = 64;
   canvas.height = 64;
-  const ratio  = Math.max(canvas.width / bitmap.width, canvas.height / bitmap.height);
+  const ratio = Math.max(canvas.width / bitmap.width, canvas.height / bitmap.height);
   const width = Math.min(bitmap.width, canvas.width / ratio);
   const height = Math.min(bitmap.height, canvas.height / ratio);
   const x = (bitmap.width - width) / 2;
@@ -26,12 +65,12 @@ const loop = async (connMap) => {
   try {
     const dataUrl = await takePhoto();
     const json = {
-      id: params.me,
-      name: params.me,
+      myself: params.myself,
+      members: params.members,
       img: dataUrl,
     };
     const data = JSON.stringify(json);
-    Object.keys(connMap).forEach(key => {
+    Object.keys(connMap).forEach((key) => {
       const conn = connMap[key];
       if (conn.open) {
         conn.send(data);
@@ -42,11 +81,11 @@ const loop = async (connMap) => {
     loop(connMap);
   } catch (e) {
     alert('Unable to capture webcam, force reloading.');
-    location.reload();
+    window.location.reload();
   }
 };
 
-const getImageEle = id => {
+const getImageEle = (id) => {
   const ele = document.getElementById('img-' + id);
   if (ele) return ele;
   const newEle = document.createElement('img');
@@ -55,40 +94,58 @@ const getImageEle = id => {
   return newEle;
 };
 
-const receiver = async data => {
+const receiver = (conn, connectMembers) => async (data) => {
   try {
     const json = JSON.parse(data);
-    getImageEle(json.id).src = json.img;
+    getImageEle(conn.peer).src = json.img;
+    if (Array.isArray(params.members) && Array.isArray(json.members)) {
+      let memberUpdated = false;
+      json.members.forEach((member) => {
+        if (!params.members.includes(member)) {
+          params.members.push(member);
+          memberUpdated = true;
+        }
+      });
+      if (memberUpdated) {
+        updateParams();
+        connectMembers();
+      }
+    }
   } catch (e) {
     console.log('receiver', e);
   }
 };
 
-const forceArray = x => x ? (Array.isArray(x) ? x : [x]) : [];
-
 const main = async () => {
-  const me = params.namespace + '_' + params.me;
-  const peer = new Peer(me, {
+  const roomid = getRoomid();
+  const myself = getMyself();
+  const peer = new Peer(roomid + '_' + myself, {
     host: 'peerjs.axlight.com',
-    port: location.protocol === 'https:' ? 443 : 80,
-    secure: location.protocol === 'https:',
+    port: window.location.protocol === 'https:' ? 443 : 80,
+    secure: window.location.protocol === 'https:',
   });
-  peer.on('error', async err => {
+  peer.on('error', async (err) => {
     if (err.type === 'peer-unavailable') return;
     console.error('main', err.type, err, peer);
     peer.destroy();
     alert('Fatal Error: check console and contact admin, then reload to start over.');
   });
   const connMap = {};
-  forceArray(params.friends).forEach(x => {
-    const friend = params.namespace + '_' + x;
-    const conn = peer.connect(friend);
+  const connectMembers = () => {
+    const members = getMembers();
+    members.forEach((member) => {
+      if (member === myself) return;
+      const id = roomid + '_' + member;
+      if (connMap[id]) return;
+      const conn = peer.connect(id);
+      connMap[id] = conn;
+      conn.on('data', receiver(conn, connectMembers));
+    });
+  };
+  connectMembers();
+  peer.on('connection', (conn) => {
     connMap[conn.peer] = conn;
-    conn.on('data', receiver);
-  });
-  peer.on('connection', conn => {
-    connMap[conn.peer] = conn;
-    conn.on('data', receiver);
+    conn.on('data', receiver(conn, connectMembers));
   });
   await sleep(1000);
   loop(connMap);
