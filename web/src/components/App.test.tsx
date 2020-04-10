82819a8b4af9eadb89ea21b559d0e82ae2d5a3b7
daishi <daishi@axlight.com>
2020-04-11 01:15:30 +0900

    yarn test

--- web/src/components/App.test.tsx
+++ web/src/components/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/your name/i);
   expect(linkElement).toBeInTheDocument();
 });
