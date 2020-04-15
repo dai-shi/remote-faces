2b3bc80d9a72f2b4e7f8e7c05c7fc5bf517e9929
dai-shi <dai-shi@users.noreply.github.com>
2020-04-15 23:57:16 +0000

    deploy: 7b13974d6d4d31d1629a34585f0e453c64dcde72

---
+++ d/2020-04-15/precache-manifest.4b52f0a84b303d8911d5363ec5c9c337.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20b642ac6935637693dbb04d786039e2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2c47b12f3db2b8fdce82",
+    "url": "./static/css/main.db99a33c.chunk.css"
+  },
+  {
+    "revision": "068a623277e4476df2fd",
+    "url": "./static/js/2.9fc85bb2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.9fc85bb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c47b12f3db2b8fdce82",
+    "url": "./static/js/main.dd6f4f69.chunk.js"
+  },
+  {
+    "revision": "951db09894589c246f83",
+    "url": "./static/js/runtime-main.fa19be48.js"
+  }
+]);
