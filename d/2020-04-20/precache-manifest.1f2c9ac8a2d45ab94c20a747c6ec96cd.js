cee955421135ca28bd4a70747d7aa50fbe03e38b
dai-shi <dai-shi@users.noreply.github.com>
2020-04-20 13:48:12 +0000

    deploy: a4ece3e8419f3d0f1509f2510a9406133aefa9a6

---
+++ d/2020-04-20/precache-manifest.1f2c9ac8a2d45ab94c20a747c6ec96cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "772f02f3682c52eb046b02d9cca49a85",
+    "url": "./index.html"
+  },
+  {
+    "revision": "887d17708b44c672e1ad",
+    "url": "./static/css/main.ec261437.chunk.css"
+  },
+  {
+    "revision": "98b9a20c0c610dc3d36f",
+    "url": "./static/js/2.3cb89571.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.3cb89571.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "887d17708b44c672e1ad",
+    "url": "./static/js/main.f61f1c44.chunk.js"
+  },
+  {
+    "revision": "951db09894589c246f83",
+    "url": "./static/js/runtime-main.fa19be48.js"
+  }
+]);
--- d/2020-04-20/service-worker.js
+++ d/2020-04-20/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2acb393dd44c0af3b460aeead41d45b8.js"
+  "./precache-manifest.1f2c9ac8a2d45ab94c20a747c6ec96cd.js"
 );
 
 self.addEventListener('message', (event) => {
