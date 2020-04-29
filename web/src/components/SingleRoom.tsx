03a8813c791493197a133827aba401da3a22136b
Teruhisa Yamamoto <t.yamamoto28@gmail.com>
2020-04-30 07:48:23 +0900

    add user profile area and modify styles (#80)

--- web/src/components/SingleRoom.tsx
+++ web/src/components/SingleRoom.tsx
@@ -10,12 +10,8 @@ import MomentaryChat from "./MomentaryChat";
 import ScreenShare from "./ScreenShare";
 import VideoShare from "./VideoShare";
 import CollabWhiteBoard from "./CollabWhiteBoard";
-import {
-  Emoji,
-  EmojiPicker,
-  EmojiDataType,
-  isEmojiDataType,
-} from "../utils/emoji";
+import UserProfile from "./UserProfile";
+import { EmojiDataType } from "../utils/emoji";
 
 type Props = {
   roomId: string;
@@ -27,45 +23,10 @@ const initialConfigOpen = getStringItem("config_hidden") !== "true";
 const initialVideoDeviceId = getStringItem("faceimage_video_device_id");
 const initialAudioDeviceId = getStringItem("faceimage_audio_device_id");
 
-const TextField = React.memo<{
-  initialText: string;
-  onUpdate: (text: string) => void;
-  buttonLabel?: string;
-  placeholder?: string;
-  clearOnUpdate?: boolean;
-}>(({ initialText, onUpdate, buttonLabel, placeholder, clearOnUpdate }) => {
-  const [text, setText] = useState(initialText);
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    if (text) {
-      onUpdate(text);
-      if (clearOnUpdate) {
-        setText("");
-      }
-    }
-  };
-
-  return (
-    <form onSubmit={onSubmit}>
-      <input
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        placeholder={placeholder}
-      />
-      {buttonLabel && (
-        <button type="submit" disabled={!text}>
-          {buttonLabel}
-        </button>
-      )}
-    </form>
-  );
-});
-
 const SingleRoom: React.FC<Props> = ({ roomId, userId }) => {
   const [nickname, setNickname] = useState(initialNickname);
   const [statusMesg, setStatusMesg] = useState("");
   const [emoji, setEmoji] = useState<EmojiDataType | null>(null);
-  const [openEmojiPicker, setOpenEmojiPicker] = useState(false);
   useEffect(() => {
     setRoomIdToUrl(roomId);
   }, [roomId]);
@@ -95,159 +56,6 @@ const SingleRoom: React.FC<Props> = ({ roomId, userId }) => {
 
   return (
     <>
-      <div className="SingleRoom-status">{JSON.stringify(networkStatus)}</div>
-      <button
-        type="button"
-        className="SingleRoom-config-toggle"
-        onClick={() => setConfigOpen((o) => !o)}
-      >
-        {configOpen ? <>&#9660;</> : <>&#9654;</>}
-      </button>
-      {configOpen && (
-        <div className="SingleRoom-config">
-          <div>
-            Link to this room:
-            <input value={window.location.href} readOnly />
-            (Share this link with your colleagues)
-            <a href={appLink}>Open App</a>
-          </div>
-          <div className="SingleRoom-nickname">
-            Your Name:{" "}
-            <TextField
-              initialText={initialNickname}
-              onUpdate={(text) => {
-                setNickname(text);
-                setStringItem("nickname", text);
-              }}
-              placeholder="Enter your name"
-              buttonLabel="Set"
-            />
-          </div>
-          <div className="SingleRoom-statusmesg">
-            Your Status:{" "}
-            <TextField
-              initialText=""
-              onUpdate={(text) => {
-                setStatusMesg(text);
-              }}
-              placeholder="Enter status message"
-              buttonLabel="Set"
-            />
-          </div>
-          <div className="SingleRoom-emoji">
-            Your Emoji:{" "}
-            <button
-              type="button"
-              onClick={() => {
-                setOpenEmojiPicker(!openEmojiPicker);
-              }}
-            >
-              {emoji ? <Emoji emoji={emoji} size={16} /> : "(Not Set)"}
-            </button>
-            <button
-              type="button"
-              onClick={() => {
-                setEmoji(null);
-                setOpenEmojiPicker(false);
-              }}
-            >
-              Clear
-            </button>
-          </div>
-          {openEmojiPicker && (
-            <EmojiPicker
-              onSelect={(e) => {
-                if (isEmojiDataType(e)) {
-                  setEmoji(e);
-                }
-                setOpenEmojiPicker(false);
-              }}
-            />
-          )}
-          <div>
-            Select Camera:{" "}
-            <select
-              value={videoDeviceId}
-              onChange={(e) => {
-                setVideoDeviceId(e.target.value);
-                setStringItem("faceimage_video_device_id", e.target.value);
-              }}
-            >
-              {videoDevices.map((videoDevice) => (
-                <option key={videoDevice.deviceId} value={videoDevice.deviceId}>
-                  {videoDevice.label}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div>
-            Select Mic:{" "}
-            <select
-              value={audioDeviceId}
-              onChange={(e) => {
-                setAudioDeviceId(e.target.value);
-                setStringItem("faceimage_audio_device_id", e.target.value);
-              }}
-            >
-              {audioDevices.map((audioDevice) => (
-                <option key={audioDevice.deviceId} value={audioDevice.deviceId}>
-                  {audioDevice.label}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div>
-            Live Mode:{" "}
-            <button type="button" onClick={() => setLiveMode((x) => !x)}>
-              {liveMode
-                ? "Disable Live Mode (currently on)"
-                : "Enable Live Mode (currently off)"}
-            </button>
-            {liveMode && (
-              <>
-                <label>
-                  <input
-                    type="checkbox"
-                    checked={micOn}
-                    onChange={(e) => setMicOn(e.target.checked)}
-                  />
-                  Mic On
-                </label>
-                <label>
-                  <input
-                    type="checkbox"
-                    checked={speakerOn}
-                    onChange={(e) => setSpeakerOn(e.target.checked)}
-                  />
-                  Speaker On
-                </label>
-              </>
-            )}
-          </div>
-          <div>
-            Screen Share:{" "}
-            <button type="button" onClick={() => setScreenShareMode((x) => !x)}>
-              {screenShareMode
-                ? "Disable Screen Share (currently on)"
-                : "Enable Screen Share (currently off)"}
-            </button>
-          </div>
-          <div>
-            Video Share:{" "}
-            <button type="button" onClick={() => setVideoShareMode((x) => !x)}>
-              {videoShareMode
-                ? "Disable Video Share (currently on)"
-                : "Enable Video Share (currently off)"}
-            </button>
-          </div>
-          <div>
-            Collab White Board:{" "}
-            <button type="button" onClick={() => setCollabWBOpen((x) => !x)}>
-              {collabWBOpen ? "Close" : "Open"}
-            </button>
-          </div>
-        </div>
-      )}
       <div className="SingleRoom-body">
         <FaceImages
           roomId={roomId}
@@ -260,7 +68,141 @@ const SingleRoom: React.FC<Props> = ({ roomId, userId }) => {
           micOn={micOn}
           speakerOn={speakerOn}
         />
-        <MomentaryChat roomId={roomId} userId={userId} nickname={nickname} />
+        <div>
+          <UserProfile
+            initialNickname={initialNickname}
+            emoji={emoji}
+            onUpdateNickname={(text) => {
+              setNickname(text);
+              setStringItem("nickname", text);
+            }}
+            onUpdateStatusMesg={(text) => {
+              setStatusMesg(text);
+            }}
+            onUpdateEmoji={(e) => {
+              setEmoji(e);
+            }}
+          />
+          <button
+            type="button"
+            className="SingleRoom-config-toggle"
+            onClick={() => setConfigOpen((o) => !o)}
+          >
+            Setting{configOpen ? <>&#9660;</> : <>&#9654;</>}
+          </button>
+          {configOpen && (
+            <div className="SingleRoom-config">
+              <div>
+                Link to this room:
+                <input value={window.location.href} readOnly />
+                (Share this link with your colleagues)
+                <a href={appLink}>Open App</a>
+              </div>
+              <div>
+                Select Camera:{" "}
+                <select
+                  value={videoDeviceId}
+                  onChange={(e) => {
+                    setVideoDeviceId(e.target.value);
+                    setStringItem("faceimage_video_device_id", e.target.value);
+                  }}
+                >
+                  {videoDevices.map((videoDevice) => (
+                    <option
+                      key={videoDevice.deviceId}
+                      value={videoDevice.deviceId}
+                    >
+                      {videoDevice.label}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div>
+                Select Mic:{" "}
+                <select
+                  value={audioDeviceId}
+                  onChange={(e) => {
+                    setAudioDeviceId(e.target.value);
+                    setStringItem("faceimage_audio_device_id", e.target.value);
+                  }}
+                >
+                  {audioDevices.map((audioDevice) => (
+                    <option
+                      key={audioDevice.deviceId}
+                      value={audioDevice.deviceId}
+                    >
+                      {audioDevice.label}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div>
+                Live Mode:{" "}
+                <button type="button" onClick={() => setLiveMode((x) => !x)}>
+                  {liveMode
+                    ? "Disable Live Mode (currently on)"
+                    : "Enable Live Mode (currently off)"}
+                </button>
+                {liveMode && (
+                  <>
+                    <label>
+                      <input
+                        type="checkbox"
+                        checked={micOn}
+                        onChange={(e) => setMicOn(e.target.checked)}
+                      />
+                      Mic On
+                    </label>
+                    <label>
+                      <input
+                        type="checkbox"
+                        checked={speakerOn}
+                        onChange={(e) => setSpeakerOn(e.target.checked)}
+                      />
+                      Speaker On
+                    </label>
+                  </>
+                )}
+              </div>
+              <div>
+                Screen Share:{" "}
+                <button
+                  type="button"
+                  onClick={() => setScreenShareMode((x) => !x)}
+                >
+                  {screenShareMode
+                    ? "Disable Screen Share (currently on)"
+                    : "Enable Screen Share (currently off)"}
+                </button>
+              </div>
+              <div>
+                Video Share:{" "}
+                <button
+                  type="button"
+                  onClick={() => setVideoShareMode((x) => !x)}
+                >
+                  {videoShareMode
+                    ? "Disable Video Share (currently on)"
+                    : "Enable Video Share (currently off)"}
+                </button>
+              </div>
+              <div>
+                Collab White Board:{" "}
+                <button
+                  type="button"
+                  onClick={() => setCollabWBOpen((x) => !x)}
+                >
+                  {collabWBOpen ? "Close" : "Open"}
+                </button>
+              </div>
+              <div className="SingleRoom-status">
+                {JSON.stringify(networkStatus)}
+              </div>
+            </div>
+          )}
+          <hr />
+          <MomentaryChat roomId={roomId} userId={userId} nickname={nickname} />
+        </div>
         {screenShareMode && (
           <ScreenShare roomId={roomId} userId={userId} nickname={nickname} />
         )}
---
+++ web/src/components/UserProfile.tsx
@@ -0,0 +1,113 @@
+import React, { useState } from "react";
+
+import "./UserProfile.css";
+import {
+  Emoji,
+  EmojiPicker,
+  EmojiDataType,
+  isEmojiDataType,
+} from "../utils/emoji";
+
+const TextField = React.memo<{
+  initialText: string;
+  onUpdate: (text: string) => void;
+  buttonLabel?: string;
+  placeholder?: string;
+  clearOnUpdate?: boolean;
+}>(({ initialText, onUpdate, buttonLabel, placeholder, clearOnUpdate }) => {
+  const [text, setText] = useState(initialText);
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (text) {
+      onUpdate(text);
+      if (clearOnUpdate) {
+        setText("");
+      }
+    }
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder={placeholder}
+      />
+      {buttonLabel && (
+        <button type="submit" disabled={!text}>
+          {buttonLabel}
+        </button>
+      )}
+    </form>
+  );
+});
+
+const UserProfile: React.FC<{
+  initialNickname: string;
+  emoji: EmojiDataType | null;
+  onUpdateNickname: (e: string) => void;
+  onUpdateStatusMesg: (e: string) => void;
+  onUpdateEmoji: (e: EmojiDataType | null) => void;
+}> = ({
+  initialNickname,
+  emoji,
+  onUpdateNickname,
+  onUpdateStatusMesg,
+  onUpdateEmoji,
+}) => {
+  const [openEmojiPicker, setOpenEmojiPicker] = useState(false);
+  return (
+    <div className="UserProfile-nickname">
+      <div className="UserProfile-nickname">
+        <TextField
+          initialText={initialNickname}
+          onUpdate={onUpdateNickname}
+          placeholder="Enter your name"
+          buttonLabel="Set"
+        />
+      </div>
+      <div className="UserProfile-status-area">
+        <div className="UserProfile-emoji">
+          <button
+            type="button"
+            onClick={() => {
+              setOpenEmojiPicker(!openEmojiPicker);
+            }}
+          >
+            {emoji ? <Emoji emoji={emoji} size={10} /> : ":)"}
+          </button>
+        </div>
+        <div className="UserProfile-statusmesg">
+          <TextField
+            initialText=""
+            onUpdate={onUpdateStatusMesg}
+            placeholder="Enter status message"
+            buttonLabel="Set"
+          />
+        </div>
+        <button
+          type="button"
+          onClick={() => {
+            onUpdateEmoji(null);
+            onUpdateStatusMesg("");
+            setOpenEmojiPicker(false);
+          }}
+        >
+          Clear
+        </button>
+      </div>
+      {openEmojiPicker && (
+        <EmojiPicker
+          onSelect={(e) => {
+            if (isEmojiDataType(e)) {
+              onUpdateEmoji(e);
+            }
+            setOpenEmojiPicker(false);
+          }}
+        />
+      )}
+    </div>
+  );
+};
+
+export default UserProfile;
