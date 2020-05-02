61f7dbc5da2e0e5fb80aeaf9b847e41a74e8dafc
daishi <daishi@axlight.com>
2020-05-02 10:51:38 +0900

    chore: emoji util typing

--- web/src/utils/emoji.ts
+++ web/src/utils/emoji.ts
@@ -1,17 +1,14 @@
+import React from "react";
 import "emoji-mart/css/emoji-mart.css";
-import { BaseEmoji } from "emoji-mart";
+import { BaseEmoji, Picker } from "emoji-mart";
 
-export { Emoji, Picker as EmojiPicker } from "emoji-mart";
+export { Emoji } from "emoji-mart";
 export type EmojiDataType = BaseEmoji;
 
-export const isEmojiDataType = (
-  emojiData: unknown
-): emojiData is EmojiDataType => {
-  try {
-    const { id } = emojiData as EmojiDataType;
-    if (typeof id === "string") return true;
-    return false;
-  } catch (e) {
-    return false;
-  }
-};
+// we do not support custom emojis
+export const EmojiPicker = Picker as React.ComponentType<
+  | Omit<React.ComponentProps<typeof Picker>, "custom" | "onSelect">
+  | {
+      onSelect: (emoji: BaseEmoji) => void;
+    }
+>;
