3905d35b06a790098239d07177f50d938dd427e1
Daishi Kato <dai-shi@users.noreply.github.com>
2020-05-09 23:05:32 +0900

    Improve live mode connectivity (#102)
    * fix: renegotiate on failure in network
    
    * track ended emulation with transceiver.currentDirection

--- web/src/network/trackUtils.ts
+++ web/src/network/trackUtils.ts
@@ -1,33 +1,32 @@
+import { sleep } from "../utils/sleep";
+
 const setupMap = new WeakMap<MediaStreamTrack, boolean>();
 
 // XXX we don't get "ended" event with removeTrack,
-// so a workaround with "mute" but "mute" is dispatched occasionally,
-// so use this timeout hack
-export const setupTrackStopOnLongMute = (track: MediaStreamTrack) => {
+// so a workaround is onmute and transceiver.currentDirection
+export const setupTrackStopOnLongMute = (
+  track: MediaStreamTrack,
+  pc: RTCPeerConnection
+) => {
   if (setupMap.has(track)) {
     return track;
   }
   setupMap.set(track, true);
-  let timeout: NodeJS.Timeout;
-  const onmute = () => {
-    clearTimeout(timeout);
-    timeout = setTimeout(() => {
+  const onmute = async () => {
+    await sleep(5000);
+    const transceiver = pc
+      .getTransceivers()
+      .find((t) => t.receiver.track === track);
+    if (
+      transceiver &&
+      (transceiver.currentDirection === "inactive" ||
+        transceiver.currentDirection === "sendonly")
+    ) {
       track.stop();
       // XXX we need to manually dispatch ended event, why?
       track.dispatchEvent(new Event("ended"));
-    }, 5000);
-  };
-  const onunmute = () => {
-    clearTimeout(timeout);
+    }
   };
   track.addEventListener("mute", onmute);
-  track.addEventListener("unmute", onunmute);
-  // Do we need this cleanup?
-  track.addEventListener("ended", () => {
-    setupMap.delete(track);
-    clearTimeout(timeout);
-    track.removeEventListener("mute", onmute);
-    track.removeEventListener("unmute", onunmute);
-  });
   return track;
 };
