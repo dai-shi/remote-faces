dd32f377098a77dd32643d1854473d9ea2b706ea
Daishi Kato <dai-shi@users.noreply.github.com>
2020-06-13 06:45:47 +0900

    Update main.js

--- main.js
+++ main.js
@@ -350,7 +350,15 @@ const initParams = () => new Promise((resolve) => {
 
 const main = async () => {
   if (!params.roomid || !params.myself) {
-    window.location.href = 'https://github.com/dai-shi/remote-faces';
+    fetch('https://api.github.com/repos/dai-shi/remote-faces/git/trees/gh-pages')
+      .then(res => res.json())
+      .then(json => json.tree.find(x => x.path === 'd').url)
+      .then(url => fetch(url))
+      .then(res => res.json())
+      .then(json => {
+        const url = 'https://remote-faces.js.org/d/' + json.tree.pop().path + '/';
+        window.location.href = url;
+      });
     return;
     // await initParams();
   }
