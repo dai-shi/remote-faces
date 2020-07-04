14332bf4785f38c8d72c1ae8d27af47896dfded3
daishi <daishi@axlight.com>
2020-07-04 21:12:37 +0900

    fix test

--- web/src/components/App.test.tsx
+++ web/src/components/App.test.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, waitForElementToBeRemoved } from "@testing-library/react";
 import { App } from "./App";
 
 jest.mock("../utils/crypto");
 
-test("renders App", () => {
-  const { getByText } = render(<App />);
-  const element = getByText(/Loading|Create a new room/i);
-  expect(element).toBeInTheDocument();
+test("renders App", async () => {
+  const { getByText, getAllByText } = render(<App />);
+  await waitForElementToBeRemoved(getByText(/loading/i));
+  const elements = getAllByText(/create a new room/i);
+  expect(elements.length).toBeGreaterThanOrEqual(1);
 });
