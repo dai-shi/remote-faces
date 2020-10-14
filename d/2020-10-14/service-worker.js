e62bc1985dc15bd1fe4a771665b9849ddfdc3e8c
dai-shi <dai-shi@users.noreply.github.com>
2020-10-14 11:24:01 +0000

    deploy: 57f095e327d989758f6aa072534a78b036f4279a

--- d/2020-10-14/service-worker.js
+++ d/2020-10-14/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.35047b8369ed35021d5dd036f7700c86.js"
+  "./precache-manifest.82cb24d63b57837e2edba83e4ab9da0e.js"
 );
 
 self.addEventListener('message', (event) => {
