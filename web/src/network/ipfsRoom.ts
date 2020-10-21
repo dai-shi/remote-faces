4055fdf4bac77162761da1ff2c32ec5e1c8d7551
daishi <daishi@axlight.com>
2020-10-21 16:20:44 +0900

    fix: test ipfs room config

--- web/src/network/ipfsRoom.ts
+++ web/src/network/ipfsRoom.ts
@@ -37,7 +37,7 @@ export const createRoom: CreateRoom = async (
     config: {
       Addresses: {
         Swarm: [
-          "/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star/",
+          // "/dns4/wrtc-star1.par.dwebops.pub/tcp/443/wss/p2p-webrtc-star/",
           "/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star/",
         ],
       },
