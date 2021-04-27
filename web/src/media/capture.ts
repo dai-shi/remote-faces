5d1d2a4deef074caee61f7f73f52b9de0be08ff3
daishi <daishi@axlight.com>
2021-04-28 00:48:07 +0900

    fix: specifying aspect ratio in take photo

--- web/src/media/capture.ts
+++ web/src/media/capture.ts
@@ -37,8 +37,10 @@ const captureImage = async (stream: MediaStream, track: MediaStreamTrack) => {
 
 export const takePhoto = async (deviceId?: string) => {
   const constraints = {
-    video: deviceId ? { deviceId } : true,
-    aspectRatio: 16 / 9,
+    video: {
+      deviceId,
+      aspectRatio: 16 / 9,
+    },
   };
   const stream = await navigator.mediaDevices.getUserMedia(constraints);
   const [track] = stream.getVideoTracks();
