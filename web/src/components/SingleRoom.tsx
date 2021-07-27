360678c59888c988c768ca6db53d46226466a6b1
daishi <daishi@axlight.com>
2021-07-27 17:44:13 +0900

    detect inactivity and raise error

--- web/src/components/SingleRoom.tsx
+++ web/src/components/SingleRoom.tsx
@@ -13,6 +13,24 @@ export const SingleRoom = React.memo(() => {
     setRoomIdToUrl(roomId);
   }, [roomId]);
 
+  const [inactive, setInactive] = useState(false);
+  if (inactive) {
+    throw new Error("inactive for too long");
+  }
+  useEffect(() => {
+    let lastTime = Date.now();
+    const timer = setInterval(() => {
+      const now = Date.now();
+      if (now - lastTime >= 4 * 60 * 60 * 1000) {
+        setInactive(true);
+      }
+      lastTime = now;
+    }, 60 * 60 * 1000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
   return (
     <div className="SingleRoom-container">
       <GatherArea
