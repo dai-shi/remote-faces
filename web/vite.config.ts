5b798d13604c051ce9271e7b3f3e49d88dc85a7b
daishi <daishi@axlight.com>
2022-11-05 12:42:01 +0900

    node globals polyfill

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,9 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import NodeGlobalsPolyfillPlugin from "@esbuild-plugins/node-globals-polyfill";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: "window",
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+      ],
+    },
+  },
   build: {
     outDir: "build",
   },
